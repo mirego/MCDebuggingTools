54a40fa460c5b26417318e1331b2dddc23b87308
Marc Lefrançois <mlefrancois@mirego.com>
2013-03-13 14:10:19 -0400

    Add podspec, license and source files
    Signed-off-by: Marc Lefrançois <mlefrancois@mirego.com>

---
+++ MCDebuggingTools.podspec
@@ -0,0 +1,13 @@
+Pod::Spec.new do |s|
+  s.name     = 'MCDebuggingTools'
+  s.version  = '0.1.0'
+  s.license  = 'BSD 3-Clause'
+  s.summary  = 'Various debugging tools to make your life easier.'
+  s.homepage = 'https://github.com/mirego/MCDebuggingTools.iOS.git'
+  s.authors  = { 'Mirego, Inc.' => 'info@mirego.com' }
+  s.source   = { :git => 'https://github.com/mirego/MCDebuggingTools.iOS.git', :tag => '0.1.0' }
+  s.source_files = 'MCDebuggingTools/*.{h,m}'
+  s.requires_arc = true
+  s.frameworks = 'QuartzCore'
+  s.platform = :ios, '5.0'
+end
---
+++ MCDebuggingTools/MCMemoryWarningGenerator.h
@@ -0,0 +1,104 @@
+//
+//  MCMemoryWarningGenerator.h
+//  Mechanics.IOS
+//
+//  Created by Francois Lambert on 11-12-23.
+//  Copyright (c) 2012 Mirego Inc. All rights reserved.
+//
+
+#import <mach/mach.h>
+#import <mach/mach_host.h>
+
+#ifdef DEBUG
+
+#define PERFORM_MEMORY_WARNING_AT_INTERVAL(interval) \
+    [NSTimer scheduledTimerWithTimeInterval:interval target:[MCMemoryWarningGenerator class] selector:@selector(performMemoryWarning) userInfo:nil repeats:YES];
+
+#if !TARGET_IPHONE_SIMULATOR
+#define ALLOCATE_MEMORY_BLOCK_AT_INTERNAL(interval) \
+    [NSTimer scheduledTimerWithTimeInterval:interval target:[MCMemoryWarningGenerator sharedInstance] selector:@selector(allocateMemoryBlock) userInfo:nil repeats:YES];
+#else
+#define ALLOCATE_MEMORY_BLOCK_AT_INTERNAL(interval)
+#endif
+
+@interface MCMemoryWarningGenerator : NSObject
++ (MCMemoryWarningGenerator *)sharedInstance;
++ (void)performMemoryWarning;
+- (void)allocateMemoryBlock;
+@end
+
+@interface MCMemoryWarningGenerator ()
+@property (atomic, strong) NSMutableArray* memoryBlocks;
+@end
+
+@implementation MCMemoryWarningGenerator
+@synthesize memoryBlocks;
+
++ (MCMemoryWarningGenerator *)sharedInstance {
+    static MCMemoryWarningGenerator* instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[MCMemoryWarningGenerator alloc] init];
+        instance.memoryBlocks = [[NSMutableArray alloc] init];
+        [[NSNotificationCenter defaultCenter] addObserver:instance selector:@selector(memoryWarning:) name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
+    });
+    
+    return instance;
+}
+
++ (void)performMemoryWarning {
+#if !TARGET_IPHONE_SIMULATOR
+    [[UIApplication sharedApplication] performSelector:@selector(_performMemoryWarning)];
+#else
+    [[NSNotificationCenter defaultCenter] postNotificationName:UIApplicationDidReceiveMemoryWarningNotification object:[UIApplication sharedApplication]];
+#endif
+}
+
+- (void)allocateMemoryBlock {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        size_t block_size = 5 * (1024 * 1024);
+        void* block = malloc(block_size);
+        if (block != NULL) {
+            bzero(block, block_size);
+            [self.memoryBlocks addObject:[NSData dataWithBytesNoCopy:block length:block_size freeWhenDone:YES]];
+        }
+    });
+}
+
+- (void)memoryWarning:(NSNotification *)notification {
+    vm_size_t pagesize;
+    mach_port_t host_port = mach_host_self();
+    mach_msg_type_number_t host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
+    host_page_size(host_port, &pagesize); 
+    
+    // CGFloat MB = (1024.0f * 1024.0f);
+    vm_statistics_data_t vm_stat;
+    if (host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size) != KERN_SUCCESS) {
+        NSLog(@"Failed to fetch vm statistics");
+    } else {
+        // natural_t mem_used = (vm_stat.active_count + vm_stat.inactive_count + vm_stat.wire_count) * pagesize;
+        // natural_t mem_free = vm_stat.free_count * pagesize;
+        // natural_t mem_total = mem_used + mem_free;
+        // NSLog(@"[B] Received memory warning - used: %.2f MB free: %.2f MB total: %.2f MB", mem_used/MB, mem_free/MB, mem_total/MB);
+    }
+    
+    [self.memoryBlocks removeAllObjects];
+    
+    if (host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size) != KERN_SUCCESS) {
+        NSLog(@"Failed to fetch vm statistics");
+    } else {
+        // natural_t mem_used = (vm_stat.active_count + vm_stat.inactive_count + vm_stat.wire_count) * pagesize;
+        // natural_t mem_free = vm_stat.free_count * pagesize;
+        // natural_t mem_total = mem_used + mem_free;
+        // NSLog(@"[A] Received memory warning - used: %.2f MB free: %.2f MB total: %.2f MB", mem_used/MB, mem_free/MB, mem_total/MB);
+    }
+}
+
+@end
+
+#else
+
+#define PERFORM_MEMORY_WARNING_AT_INTERVAL(interval)
+#define ALLOCATE_MEMORY_BLOCK_AT_INTERNAL(interval)
+
+#endif
